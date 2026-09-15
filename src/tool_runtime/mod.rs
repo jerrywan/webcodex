3135087831b6ae57ea43f7a9c6558d4de2588275
yyjeqhc <1772413353@qq.com>
2026-09-16 07:47:02 +0800

    Project Code Mode composition in Runtime Console

--- src/tool_runtime/mod.rs
+++ src/tool_runtime/mod.rs
@@ -13,6 +13,8 @@ mod changes;
 mod checkpoint;
 #[cfg(feature = "experimental-code-mode")]
 mod code_mode;
+#[cfg(feature = "experimental-code-mode")]
+pub(crate) use code_mode::is_admitted_nested_tool as code_mode_nested_tool_is_admitted;
 mod coding_agent;
 mod coding_task;
 mod coding_task_tools;
